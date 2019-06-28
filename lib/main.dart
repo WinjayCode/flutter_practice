9d86263ab550fc87b9a67b36f78d03d1faea3a72
wenjie.duan <19900429>
2019-06-28 16:18:20 +0800

    提交flutter新学习

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,154 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    title: 'Shopping App',
-    home: new ShoppingList(
-      products: <Product>[
-        new Product(name: 'Eggs'),
-        new Product(name: 'Flour'),
-        new Product(name: 'Chocolate chips'),
-      ],
-    ),
-  ));
+  runApp(new MyApp2());
 }
+
+class MyApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+//    final wordPair = new WordPair.random();
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      theme: new ThemeData(
+        primaryColor: Colors.white,
+      ),
+
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text('Welcome to Flutter'),
+//        ),
+//        body: new Center(
+////          child: new Text('Hello World!'),
+////          child: new Text(wordPair.asPascalCase),
+////          child: new RandomWords(),
+//        ),
+//      ),
+
+      home: new RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+
+  final _saved = new Set<WordPair>();
+
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+        // 在偶数行，该函数会为单词对添加一个ListTile row.
+        // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+        // 注意，在小屏幕上，分割线看起来可能比较吃力。
+        itemBuilder: (context, i) {
+          // 在每一列之前，添加一个1像素高的分隔线widget
+          if (i.isOdd) return new Divider();
+          // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+          // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+          final index = i ~/ 2;
+          // 如果是建议列表中最后一个单词对
+          if (index >= _suggestions.length) {
+            // ...接着再生成10个单词对，然后添加到建议列表
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(builder: (context) {
+        final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            }
+        );
+        final divided = ListTile.divideTiles(
+          context: context,
+          tiles: tiles,
+        ).toList();
+
+        return new Scaffold(
+          appBar: new AppBar(
+            title: new Text('Saved Suggestions'),
+          ),
+          body: new ListView(children: divided),
+        );
+      })
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+//    final wordPair = new WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+}
+
+//void main() {
+//  runApp(new MaterialApp(
+//    title: 'Shopping App',
+//    home: new ShoppingList(
+//      products: <Product>[
+//        new Product(name: 'Eggs'),
+//        new Product(name: 'Flour'),
+//        new Product(name: 'Chocolate chips'),
+//      ],
+//    ),
+//  ));
+//}
 
 class Product {
   const Product({this.name});
+
   final String name;
 }
 
@@ -22,7 +156,8 @@ typedef void CartChangedCallback(Product product, bool inCart);
 
 class ShoppingListItem extends StatelessWidget {
   ShoppingListItem({Product product, this.inCart, this.onCartChanged})
-    : product = product, super(key: new ObjectKey(product));
+      : product = product,
+        super(key: new ObjectKey(product));
 
   final Product product;
   final bool inCart;
@@ -83,7 +218,6 @@ class _ShoppingListState extends State<ShoppingList> {
       appBar: new AppBar(
         title: new Text('Shopping List'),
       ),
-
       body: new ListView(
         padding: new EdgeInsets.symmetric(vertical: 8.0),
         children: widget.products.map((Product product) {
@@ -97,7 +231,6 @@ class _ShoppingListState extends State<ShoppingList> {
     );
   }
 }
-
 
 class MyButton extends StatelessWidget {
   @override
@@ -132,29 +265,25 @@ class MyButton extends StatelessWidget {
 class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold (
+    return new Scaffold(
       appBar: new AppBar(
         leading: new IconButton(
-            icon: new Icon(Icons.menu),
-            onPressed: null,
-            tooltip: 'Navigation menu',
+          icon: new Icon(Icons.menu),
+          onPressed: null,
+          tooltip: 'Navigation menu',
         ),
-
         title: new Text('Example title'),
-
         actions: <Widget>[
           new IconButton(
-              icon: new Icon(Icons.search),
-              onPressed: null,
-              tooltip: 'Search',
+            icon: new Icon(Icons.search),
+            onPressed: null,
+            tooltip: 'Search',
           )
         ],
       ),
-
       body: new Center(
         child: new Text('Hello, world!'),
       ),
-
       floatingActionButton: new FloatingActionButton(
         tooltip: 'Add',
         child: new Icon(Icons.add),
@@ -183,14 +312,10 @@ class MyScaffold extends StatelessWidget {
               style: Theme.of(context).primaryTextTheme.title,
             ),
           ),
-
           new Expanded(
               child: new Center(
-                child: new Text(
-                  'Hello World!'
-                ),
-              )
-          )
+            child: new Text('Hello World!'),
+          ))
         ],
       ),
     );
@@ -211,19 +336,15 @@ class MyAppBar extends StatelessWidget {
       child: new Row(
         children: <Widget>[
           new IconButton(
-              icon: new Icon(Icons.menu),
-              onPressed: null,
-              tooltip: 'Navigation menu',
+            icon: new Icon(Icons.menu),
+            onPressed: null,
+            tooltip: 'Navigation menu',
           ),
-
-          new Expanded(
-              child: title
-          ),
-
+          new Expanded(child: title),
           new IconButton(
-              icon: new Icon(Icons.search),
-              onPressed: null,
-              tooltip: 'Search',
+            icon: new Icon(Icons.search),
+            onPressed: null,
+            tooltip: 'Search',
           )
         ],
       ),
